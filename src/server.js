9f888b9dac79e31cb0974682b69c2541ccdeeba6
Sunset Tran <sunsettrandev@gmail.com>
2022-05-26 19:52:11 +0700

    router and mvc

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 import express from 'express'
 import configViewEngine from './configs/viewEngine'
+import initWebRoute from './route/web'
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 3000;
 // console.log(port)
 
+//
 configViewEngine(app);
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-})
+//
+initWebRoute(app);
 
 app.listen(port, () => {
   console.log(port);
