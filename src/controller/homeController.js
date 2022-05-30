1c94cd1683e4f7e1bde4892829b913fea1a86b37
Sunset Tran <sunsettrandev@gmail.com>
2022-05-30 09:10:20 +0700

    complete CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -30,6 +30,25 @@ let createNewUser = async (req, res) => {
   return res.redirect('/')
 }
 
+let deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute('delete from users where id = ?', [userId]);
+  return res.redirect('/');
+}
+
+let getEditUser = async (req, res) => {
+  let userId = req.params.userId;
+  let [user] = await pool.execute('select * from `users` where id = ?', [userId]);
+  return res.render('update.ejs', { dataUser: user[0] });
+}
+
+let updateUser = async (req, res) => {
+  let { firstName, lastName, email, address, id } = req.body
+  await pool.execute('update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?',
+    [firstName, lastName, email, address, id]);
+  return res.redirect('/');
+}
+
 module.exports = {
-  getHomePage, getDetailPage, createNewUser
+  getHomePage, getDetailPage, createNewUser, deleteUser, getEditUser, updateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,9 @@ const initWebRoute = (app) => {
   router.get('/', homeController.getHomePage)
   router.get('/detail/user/:userId', homeController.getDetailPage)
   router.post('/create-new-user', homeController.createNewUser)
+  router.get('/edit-user/:userId', homeController.getEditUser)
+  router.post('/update-user', homeController.updateUser)
+  router.post('/delete-user', homeController.deleteUser)
   return app.use('/', router)
 }
 
