c6a6116c6ded0214e7b8ee41f3d7a57cfc1794a0
Sunset Tran <sunsettrandev@gmail.com>
2022-06-06 16:39:08 +0700

    handle upload file

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,4 @@
+import multer from 'multer';
 import pool from '../configs/connectDB';
 
 let getHomePage = async (req, res) => {
@@ -49,6 +50,35 @@ let updateUser = async (req, res) => {
   return res.redirect('/');
 }
 
+let getUploadFilePage = async (req, res) => {
+  return res.render('uploadFile.ejs');
+}
+
+const upload = multer().single('profile_pic');
+
+let handleUploadFile = async (req, res) => {
+  //profile-pic  is the name of our file field in the HTML form
+  upload(req, res, function (err) {
+    //req.file contains information of uploaded file
+    //req.body contains information of text fields, if there were any
+
+    if (req.fileValidationError) {
+      return res.send(req.fileValidationError);
+    }
+    else if (!req.file) {
+      return res.send('Please select an image to upload');
+    }
+    else if (err instanceof multer.MulterError) {
+      return res.send(err);
+    }
+
+    //display uploaded image for user validation
+    res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500" /><a href="/upload">Upload another image</a>`);
+
+  });
+}
+
 module.exports = {
-  getHomePage, getDetailPage, createNewUser, deleteUser, getEditUser, updateUser
+  getHomePage, getDetailPage, createNewUser, deleteUser, getEditUser, updateUser, getUploadFilePage,
+  handleUploadFile
 }
--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,31 @@
 import express from "express";
 import homeController from "../controller/homeController"
+import path from "path";
+import multer from "multer";
+var appRoot = require('app-root-path');
 let router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log('>>>check appRoot: ', appRoot);
+    cb(null, appRoot + "/src/public/image/");
+  },
+  //by default, multer removes file extension so let's add them back
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const imageFilter = function (req, file, cb) {
+  //accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
+}
+
+const upload = multer({ storage: storage, fileFilter: imageFilter });
 
 const initWebRoute = (app) => {
   router.get('/', homeController.getHomePage)
@@ -9,6 +34,8 @@ const initWebRoute = (app) => {
   router.get('/edit-user/:userId', homeController.getEditUser)
   router.post('/update-user', homeController.updateUser)
   router.post('/delete-user', homeController.deleteUser)
+  router.get('/upload', homeController.getUploadFilePage)
+  router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
   return app.use('/', router)
 }
 
