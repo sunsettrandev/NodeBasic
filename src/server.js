f9a52184f77d9848d206634952662ae55bc44dcf
Sunset Tran <sunsettrandev@gmail.com>
2022-06-08 16:38:17 +0700

    middleware

--- src/server.js
+++ src/server.js
@@ -3,12 +3,22 @@ import configViewEngine from './configs/viewEngine'
 import initWebRoute from './route/web'
 import initAPIRoute from './route/api'
 // import connection from './configs/connectDB'
+import morgan from 'morgan'
 
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 3000;
 // console.log(port)
+
+app.use((req, res, next) => {
+  console.log('>>> Check req');
+  console.log(req.method);
+  next();
+})
+
+//logging
+app.use(morgan('combined'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,6 +31,11 @@ initWebRoute(app);
 
 //init api route
 initAPIRoute(app);
+
+//handle 404 not found
+app.use((req, res) => {
+  return res.render('404.ejs');
+})
 
 app.listen(port, () => {
   console.log(port);
